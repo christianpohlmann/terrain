c7edc0440c08c3dba0c8bb91b284d41a1110c8cf
Christian Pohlmann <christian.pohlmann@posteo.de>
2016-09-18 10:59:00 +0200

    Initial transfer of existing source code

--- README.md
+++ README.md
@@ -1,2 +1,81 @@
 # terrain
-Procedural content generation for heightmaps.
+
+![Sample image](http://nconc.de/terrain/example1_small.png)
+
+## About
+_terrain_ is an easily extensible procedural content generation tool for creating and visualizing heightmaps written in Python.
+Currently two generation and two visualization strategies are supported. The code is quite terse with fewer than 300 LOC (excluding comments and blank lines).
+### Generation stratgies
+* Linear fault algorithm: Iterative algorithm using straights to determine areas to elevate/to lower.
+* Random algorithm: Generates a non-iterative random heightmap (can produce acceptable results if erosion is enabled).
+
+### Visualization strategies
+* Hexagons: Display each tile as hexagon. Gives a smoother appearance than using rectangles.
+* Rectangles: Display each tile as rectangle.
+
+The generation strategy creates a _n_ times _m_ heightmap where each tile is put into a tile-class.
+The mapping to tile-classes is performed based on each tile's percentile (according to its height value).
+This leads to much better results than using simple normalization which might fail to exhaust the whole spectrum of available tile-classes.
+
+The visualization strategy then paints the image based on the tile-class membership.
+
+_terrain_ was written with extensibility in mind. See [Extensibility](#extensibility) for details on how to extend terrain with your own generation and visualization algorithms.
+
+## Requirements
+* Python 3.x
+* PIL
+* numpy
+
+## Installation
+Just clone this repository and run: `python3 <config>` where `<config>` refers to a configuration file where generation parameters can be specified. See [Configuration](#configuration) for details
+about available configuration options.
+
+## Configuration
+Configuration files are JSON files with a simple setup: On the first level there are two entries named `"generation"` and `"visualization"`. Their parameters are as follows:
+### generation
+* `"strategy"`: Currently either `"linear-fault"` or `"random"`.
+* `"seed"`: The seed of the random number generator.
+* `"iterations"`: How many iterations should be performed for the heightmap generation (ignored by the random generation strategy).
+* `"thresholds"`: List of numbers between 0 and 1 which determine the boundaries for tile classes.
+As mentioned above, these boundaries are not relative height data, but represent percentiles.
+* `"width"`: Number of tiles in x-direction.
+* `"height"`: Number of tiles in  y-direction.
+* `"erosion"`: Denotes how many erosion iterations should be applied.
+
+### visualization
+* `"strategy"`: Currently either `"hex"` or `"rect"`.
+* `"filename"`: Output filename of generated image.
+* `"sidelen_x"`: Width of each tile. Only used in the `"rect"` strategy.
+* `"sidelen_y"`: Height of each tile. Only used in the `"rect"` strategy.
+* `"edgelen"`: Length of a single edge of a hex tile. Only used in the `"hex"` strategy.
+* `"colors"`: List of colors to paint tiles. Each color is a list of three numbers denoting a RGB triple. Note that each tile class needs to have exactly one color,
+i.e. the length of the colors list must contain exactly one more element than the thresholds list.
+
+Sample configuration files are contained in `examples/`.
+
+
+## Extensibility
+The code for both generation and both visualization strategies is well-documented enough to understand how to write new strategies. However, a brief
+general documentation is given below.
+
+### Adding a generation strategy
+In order to add a new generation strategy is is enough to inherit from `GenerationStrategy` and override the method `_generate_raw`.
+The actual heightmap data must be stored in `self.heightmap` which is a numpy array. Percentile mapping and erosion is handled in
+the base class. After implementing a new generation strategy it must be registered in `terrain.py` inside the function `register_gen_strategies()` like this:
+
+```
+strategies['new_gen_strategy'] = NewGenStrategy
+```
+
+The strategy can now be referenced in JSON configuration files as `"new_gen_strategy"`.
+
+### Adding a visualization strategy
+Analogously to adding new generation strategies, if a new visualization strategy is desired, class `VisualizationStrategy`
+must be inherited and method `_visualize()` must be overriden. An `ImageDraw` object is provided named `self.draw`.
+Subsequently, the new visualization strategy must be registered in `terrain.py` inside the function `register_vis_strategies()` like this:
+
+```
+strategies['new_vis_strategy'] = NewVisStrategy
+```
+
+The strategy can now be referenced in JSON configuration files as `"new_vis_strategy"`.
---
+++ src/visualization.py
@@ -0,0 +1,135 @@
+#
+# Copyright (C) 2014-2016 Christian Pohlmann
+#
+# Licensed under The MIT License (see LICENSE.md)
+#
+from PIL import Image, ImageDraw, ImageFilter
+import numpy as np
+import math
+
+class VisualizationStrategy(object):
+    '''Base class for the visualization strategies. It provides some functionality
+    used by inheriting classes such as creating/saving the result image.
+    '''
+    
+    def __init__(self, width, height, heightmap, config):
+        '''Simply set the attributes which were passed as parameters.'''
+        self.width = width
+        self.height = height
+        self.heightmap = heightmap
+        self.config = config
+
+        
+    def visualize(self):
+        '''Wraps actual visualization which occurs in inheriting classes. This methods
+        sets up the result image and the color maps. Inheriting classes must implement
+        _visualize() where the actual visualization takes place and they may implement
+        _pre_save_filter() to perform some operations before saving the image.
+        '''
+        if 'filename' not in self.config:
+            raise Exception('No image filename given.')
+        filename = self.config['filename']
+
+        if 'colors' not in self.config:
+            raise Exception('No colormap given for rectangle visualization.')
+        self.color_map = self.config['colors']
+
+        self.img = Image.new('RGB', (self.width, self.height), (0,0,0))
+        self.draw = ImageDraw.Draw(self.img)
+        self._visualize()
+        self._pre_save_filter()
+        self.img.save(filename)
+
+
+    def get_color(self, idx):
+        '''Simply return the color at the given index in the color map. A conversion to
+           is necessary, because the type of idx is numpy.float64 (since it comes
+           directly from the heightmap)
+        '''
+        return self.color_map[int(idx)]
+
+    
+    def _visualize(self):
+        '''Dummy implementation of the actual visualization which takes place in the
+           subclasses.
+        '''
+        pass
+
+
+    def _pre_save_filter(self):
+        '''Dummy implementation of pre-save operations called when a subclass does not
+           override this method.
+        '''
+        pass
+
+
+class RectangleStrategy(VisualizationStrategy):
+    '''Provides a simple visualization strategy where each tile in the heightmap is
+       represented by a rectangle.
+    '''
+
+    def __init__(self, heightmap, config):
+        '''Constructor of the rectangle visualization strategy. The total width and
+        height of the image depends on the side lengths of the rectangle which must
+        be included in parameter _config_.
+        '''
+        self.sidelen_x = config['sidelen_x']
+        self.sidelen_y = config['sidelen_y']
+        width = self.sidelen_x * heightmap.shape[0]
+        height = self.sidelen_y * heightmap.shape[1]
+        super(RectangleStrategy, self).__init__(width, height, heightmap, config)
+
+
+    def _visualize(self):
+        '''Draws the individual tiles as rectangles on the image.
+        '''
+        for ((i,j), g) in np.ndenumerate(self.heightmap):
+            self.draw.rectangle([self.sidelen_x * i, self.sidelen_y * j,
+                            self.sidelen_x * (i+1), self.sidelen_y * (j+1)],
+                            fill=self.get_color(g))
+
+
+class HexagonStrategy(VisualizationStrategy):
+    '''Provides a slightly more complex visualization strategy than individual rectangles:
+    Each tile is represented by a hexagon which are aligned in a typical grid pattern.
+    '''
+
+    def __init__(self, heightmap, config):
+        '''Constructor of the hexagon visualization strategy. The total width and height
+        of the image depends on the outer edge length of an individual hexagon which
+        must be included in parameter _config_.
+        '''
+        self.edgelen = config['edgelen']
+        # note that edgelen * math.sqrt(3) is the 'inner' edge length of the hexagon
+        width = int(math.ceil(self.edgelen * math.sqrt(3) * heightmap.shape[0] +
+                    math.sqrt(3)/2 * self.edgelen))
+        height = int(math.ceil(1.5 * self.edgelen * heightmap.shape[1] + 0.5*self.edgelen))
+        super(HexagonStrategy, self).__init__(width, height, heightmap, config)
+
+
+    def _gen_hexagon(self, initial, a):
+        '''Returns all points of a individual hexagon with the top-left point being
+           parameter 'initial' and edge length 'a'
+        '''
+        x,y = initial
+        b = a * (math.sqrt(3) / 2)
+        return [(int(math.ceil(xc)), int(math.ceil(yc))) for (xc, yc) in
+                [(x,y), (x+b, y-0.5*a), (x+2*b, y),
+                 (x+2*b, y+a), (x+b, y+1.5*a), (x, y+a)]]
+
+
+    def _pre_save_filter(self):
+        '''Aplly a smooth filter to soften the edges of the hexagons.'''
+        self.img = self.img.filter(ImageFilter.SMOOTH)
+
+
+    def _visualize(self):
+        '''Draws the individual tiles as hexagons on the image.'''
+        for ((i,j), g) in np.ndenumerate(self.heightmap):
+            offset_x = int(math.ceil(math.sqrt(3)/2 * self.edgelen)) if j % 2 == 1 else 0
+            hexagon = self._gen_hexagon((int(math.ceil(i*math.sqrt(3)*
+                                                       self.edgelen+offset_x)),
+                                         int(math.ceil(j*1.5*self.edgelen + 
+                                                       0.5*self.edgelen))),
+                                        self.edgelen)
+            self.draw.polygon(hexagon, fill=self.get_color(g))
